4421a8ffe942684cc36e58c8e5fabc35bf1bd21c
apros1112 <prsolenko@yandex.ru>
2022-03-25 17:26:46 +0300

    4

--- project/script.js
+++ project/script.js
@@ -14,4 +14,26 @@ function guess() {
         return guess();
     }
 }
-guess();
+//guess();
+
+function timer() {
+    alert("ты здесь слишком долго");
+}
+
+//setTimeout(timer, 3000);
+
+function valid() {
+    var name = document.getElementById("name").value;
+    var password = document.getElementById("password").value;
+
+    var reg_name = /^[а-яa-zё]+$/i;
+  var reg_password = /^[a-z0-9]{5,100}/i;
+    if (reg_name.test(name) == false) {
+        alert("ошибка в твоем имени, либо же в тебе(");
+    }
+    if (reg_name.test(password) == false) {
+        alert("ошибка в твоем пароле, либо же в тебе(");
+    }
+
+}
+document.querySelector(".button").addEventListener("click", valid);
